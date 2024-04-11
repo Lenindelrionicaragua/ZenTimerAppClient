67a8f42cee0c2ffc179e84808b5a39e124e3ed49
lenin del rio <lenindelrionicaragua@gmail.com>
2024-04-11 12:56:34 +0200

    now expo got fast refresh any time the changes are saved

--- ZenTimer/App.js
+++ ZenTimer/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import LoginScreen from "./screens/LoginScreen/LoginScreen";
 import { AppContainer } from "./styles/AppStyles";
+import "@expo/metro-runtime";
+import { registerRootComponent } from "expo";
 
 export default function App() {
   return (
