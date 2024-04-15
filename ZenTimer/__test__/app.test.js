2968cfa75c8f308b152cf5f2e3aafdd325cca6ea
lenin del rio <lenindelrionicaragua@gmail.com>
2024-04-15 10:58:06 +0200

    refactor app.test.js

--- ZenTimer/__test__/app.test.js
+++ ZenTimer/__test__/app.test.js
@@ -1,28 +1,47 @@
 import React from "react";
+import App from "../App";
 import { render } from "@testing-library/react-native";
 import renderer from "react-test-renderer";
-import App from "../App";
 import LoginScreen from "../screens/LoginScreen/LoginScreen";
+import { AppContainer } from "../styles/AppStyles";
 
-// Should be a function
-// Should render a View element with Id 'app-container'
-// Should render the <LoginScreen/> component
+const renderApp = () => {
+  return render(<App />);
+};
 
 describe("App", () => {
-  it("should be a function", () => {
-    expect(typeof App).toBe("function");
+  // Should renders correctly AppContainer Element
+  // Should render a View element with Id 'app-container'
+  let appRenderResult;
+
+  beforeEach(() => {
+    appRenderResult = renderApp();
+  });
+
+  test("should renders correctly", () => {
+    const tree = renderer.create(<AppContainer />).toJSON();
+    expect(tree).toMatchSnapshot();
   });
 
   it("Should render a View element with the test ID 'app-container'", () => {
-    const { getByTestId } = render(<App />);
+    const { getByTestId } = appRenderResult;
     const containerElement = getByTestId("app-container");
     expect(containerElement).toBeTruthy();
   });
+
+  //LoginScreen
 
   test("Should render the LoginScreen component", () => {
     const loginScreenComponent = renderer.create(<App />);
     const instance = loginScreenComponent.root;
     const loginScreenInstance = instance.findByType(LoginScreen);
     expect(loginScreenInstance).toBeTruthy();
+  });
+});
+
+describe("LoginScreen", () => {
+  test("Should renders correctly the LoginScreen Component", () => {
+    const tree = renderer.create(<LoginScreen />).toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
